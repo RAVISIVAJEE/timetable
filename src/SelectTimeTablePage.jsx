da20019e5945096e0162d6f3aec215fda5f3bcda
RAVISIVAJEE <sivaraavi9544@gmail.com>
2024-02-26 00:17:21 +0530

    another

--- src/SelectTimeTablePage.jsx
+++ src/SelectTimeTablePage.jsx
@@ -1,27 +1,34 @@
 import React from "react";
 import Header from "./Header";
 import { NavLink } from "react-router-dom";
+import "./SelectTimeTable.css"; // Import the CSS file
 
 function SelectTimeTablePage() {
   return (
-    <div>
+    <div className="container">
+      {" "}
+      {/* Apply container class */}
       <Header />
       <h2>Select Time Table</h2>
       <div className="link-container">
         <NavLink to="/bshtimetable" className="timetable-link">
           BSH Time Table
         </NavLink>
-        <p>View the time table of BSH</p>
+        <p className="para">View the time table of BSH</p>
+      </div>
+      <br />
+      <div className="link-container">
         <NavLink to="/branchtimetable" className="timetable-link">
           Branch Time Table
         </NavLink>
-        <p>View the time table by branch</p>
+        <p className="para">View the time table by branch</p>
       </div>
+      <br />
       <div className="link-container">
         <NavLink to="/facultytimetable" className="timetable-link">
           Faculty Time Table
         </NavLink>
-        <p>View the time table by faculty</p>
+        <p className="para">View the time table by faculty</p>
       </div>
     </div>
   );
